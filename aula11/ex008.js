dcde0993a17e38121eb1f7a11a45d1489df53f3e
otaciliorezende <otaciliocunha23@gmail.com>
2022-09-29 16:46:43 -0300

    mais exercicios de js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro  e ${vel}Km/h`)
+if (vel > 80) { //condição simples!
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Dirija sempre com muito cuidado use o cinto!')
