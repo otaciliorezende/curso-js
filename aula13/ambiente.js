c987b32b73e4666ab2ed16b70f6a8873ab3b5135
otaciliorezende <otaciliocunha23@gmail.com>
2022-10-04 15:51:18 -0300

    repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+
+/*
+var c = 1
+while (c < 500) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+
