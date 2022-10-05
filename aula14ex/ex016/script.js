f26af5033c790de53f040d9ff1d11d484d99de0e
otaciliorezende <otaciliocunha23@gmail.com>
2022-10-05 16:00:17 -0300

    atualizando execrcicios

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,33 @@
-alert('Olá!')
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if (ini.Value == 0 || fim.value == 0 || passo.value == 0) {
+        res.innerHTML = 'Impossível contar!'
+        // window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>' 
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0) {
+           window.alert('Passo inválido! Considerando PASSO 1')
+           p = 1           
+        }
+        if (i < f) {
+            //Contagem Crescente
+            for(var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //Contagem Regressiva
+            for(var c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}` 
+            }
+        }      
+        res.innerHTML += `\u{1F3C1}`
+    }
+        
+}
