a38adaed6872756f55aff660c05500cf0e8a5617
otaciliorezende <otaciliocunha23@gmail.com>
2022-10-06 10:42:55 -0300

    funções em JS

---
+++ aula16/funcao01.js
@@ -0,0 +1,9 @@
+function parimp(n){
+    if (n%2 == 0){
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+//let res = parimp(4)
+console.log(parimp(234))
---
+++ aula16/funcao03.js
@@ -0,0 +1,4 @@
+let v = function(y) {
+    return y*2
+}
+console.log(v(5))
